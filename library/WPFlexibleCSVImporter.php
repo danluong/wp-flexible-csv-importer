b721e460f13bfa4072258244556db087f9854304
Leon <lionhive@gmail.com>
2017-03-13 12:58:06 +1300

    beginnings of importer class

---
+++ library/WPFlexibleCSVImporter.php
@@ -0,0 +1,27 @@
+<?php
+
+use \WP_Error;
+
+class WPFlexibleCSVImporter {
+
+	private $action = 'admin.php?import=wp-flexible-csv-importer';
+
+	public function renderMainScreen()
+	{
+        ?>
+		<form enctype="multipart/form-data" id="import-upload-form" method="post" class="wp-upload-form" action="<?php echo esc_url( add_query_arg( array( 'step' => 2 ), $this->get_action() ) ); ?>">
+			<p>
+				<label for="upload"><?php _e( 'Choose a .csv file from your computer.', 'wp-flexible-csv-importer' ); ?></label><br />(<?php printf( __('Maximum size: %s' ), $size ); ?>)
+			</p>
+			<p>
+				<input type="file" id="upload" name="import" size="25" />
+				<input type="hidden" name="action" value="save" />
+				<input type="hidden" name="max_file_size" value="<?php echo $bytes; ?>" />
+				<?php wp_nonce_field( 'acsv-import-upload' ); ?>
+			</p>
+			<?php submit_button( __('Upload file and import'), 'wp-flexible-csv-importer' ); ?>
+		</form>
+        <?php
+	}
+
+}
---
+++ wp-flexible-csv-importer.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name: WP Flexible CSV Importer
+Plugin URI:  https://leonstafford.github.io
+Description: Ease the pain of CSV imports - do it all within WP interface
+Version:     0.1
+Author:      Leon Stafford
+Author URI:  https://leonstafford.github.io
+Text Domain: wp-flexible-csv-importer
+
+Copyright (c) 2017 Leon Stafford
+ */
+
+require_once 'library/WPFlexibleCSVImporter.php';
+
+if ( is_admin() && defined('WP_LOAD_IMPORTERS') ) {
+
+	require_once ABSPATH . 'wp-admin/includes/import.php';
+
+	if ( ! class_exists( 'WP_Importer' ) ) {
+		$class_wp_importer = ABSPATH . 'wp-admin/includes/class-wp-importer.php';
+		if ( file_exists( $class_wp_importer ) ) {
+			require_once $class_wp_importer;
+		}
+	}
+
+	$wpFlexibleCSVImporter = new WPFlexibleCSVImporter();
+	register_importer(
+		'wp-flexible-csv-importer',
+		__('WP Flexible CSV Importer', 'wp-flexible-csv-importer'),
+		__('Import posts, and custom fields from any csv file.', 'wp-flexible-csv-importer'),
+		array (
+			$wpFlexibleCSVImporter,
+			'renderMainScreen'
+		)
+	);
+
+}
